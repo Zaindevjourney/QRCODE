9bf699a87197e71c669d957a285128dacb18e236
Zain_ul_Abideen <zainkhan4zzzn@gmail.com>
2024-11-11 23:29:56 +0500

    Add files via upload

---
+++ QRcode.py
@@ -0,0 +1,9 @@
+                                 # QR CODE GENERATION USING PYTHON #
+
+
+import pyqrcode
+from PIL import Image
+link = input("Enter anything to generate QR : ")
+qr_code = pyqrcode.create(link)
+qr_code.png("QRCode.png", scale=5)
+Image.open("QRCode.png")
